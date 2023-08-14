c48586026c97c29b5e1b61d3a2ba169b14931c69
Shashank-09 <shashanktulsikar979@gmail.com>
2023-08-14 23:51:52 +0530

    fifth commit

--- js/main.js
+++ js/main.js
@@ -154,7 +154,7 @@ ScrollReveal().reveal('.home .info .btn' ,  { delay:700, origin: 'bottom'});
 ScrollReveal().reveal('.media-icons i, .contacct-left li' ,  { delay:500, origin: 'left' , interval:200});
 ScrollReveal().reveal('.home-img, .about-img',  { delay:500, origin: 'bottom' });
 ScrollReveal().reveal('.about .discription, .contact-right' ,  { delay:600, origin: 'right' });
-ScrollReveal().reveal('.about .professional-list li' ,  { delay:500, origin: 'right', interval: 200 });
+ScrollReveal().reveal('.about .professional-list li' ,  { delay:400, origin: 'right', interval: 100 });
 ScrollReveal().reveal('.skills-description, .services-description, .contact-card, .contact-left h2, .contact-list',  { delay:700, origin: 'left' });
 ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio, .img-card' , { delay:800, origin: 'bottom' , interval:200 });
 ScrollReveal().reveal(' footer .group' , { delay:500, origin: 'top' , interval:200 });
